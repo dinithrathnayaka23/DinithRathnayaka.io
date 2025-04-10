0ca964896253d4ad6f93add61be3b6cd48ea6a35
dinithrathnayaka23 <rathnayakarmdno.23@uom.lk>
2025-04-10 09:54:36 +0530

    Linked to new GitHub account

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'animate.css';
-import { HashRouter } from 'react-router-dom';
-
-<HashRouter>
-  <App />
-</HashRouter>
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
